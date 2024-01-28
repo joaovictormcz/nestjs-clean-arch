097b90f18f8b9e7db82cc9216beba0de2444c93f
joaovictormcz <joaovictor.mcz@gmail.com>
2024-01-27 21:15:56 -0300

    Intial commit

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import crypto from 'crypto';
+
+export enum ProjectStatus {
+  Pending = 'pending',
+  Active = 'active',
+  Cancelled = 'cancelled',
+  Completed = 'completed',
+}
+
+@Entity()
+export class Project {
+  @PrimaryColumn()
+  id: string; //uuid
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true, type: 'datetime' })
+  started_at: Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  cancelled_at: Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  forecast_at: Date | null;
+
+  @Column({ type: 'simple-enum' })
+  status: ProjectStatus;
+
+  constructor(
+    props: {
+      name: string;
+      description: string;
+      started_at?: Date | null;
+      cancelled_at?: Date | null;
+      forecasted_at?: Date | null;
+    },
+    id?: string,
+  ) {
+    Object.assign(this, props);
+    this.id = id ?? crypto.randomUUID();
+  }
+}
---
+++ src/projects/projects.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project, ProjectStatus } from './entities/project.entity';
+
+@Injectable()
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private projectRepo: Repository<Project>,
+  ) {}
+
+  create(createProjectDto: CreateProjectDto) {
+    const project = new Project(createProjectDto);
+    if (createProjectDto.started_at) {
+      project.status == ProjectStatus.Active;
+    }
+    return this.projectRepo.save(project);
+  }
+
+  findAll() {
+    return `This action returns all projects`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} project`;
+  }
+
+  update(id: number, updateProjectDto: UpdateProjectDto) {
+    return `This action updates a #${id} project`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} project`;
+  }
+}
